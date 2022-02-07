f41081a7e0c55dfe4ae7df5d6f74d215ef7dd785
Karol Bebel <karol.bebel@allegro.pl>
2022-02-07 17:05:40 +0100

    todo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-async () => {
+(async () => {
   try {
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput("who-to-greet");
@@ -13,4 +13,4 @@ async () => {
   } catch (error) {
     core.setFailed(error.message);
   }
-};
+})()
